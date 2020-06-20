9418ee79715be6401dd4c032648334cf33b34c4c
hbcui1984 <hbcui1984@163.com>
2020-06-20 21:58:49 +0800

    引入uni-id，增加服务端逻辑

---
+++ cloudfunctions-tcb/user-center/index.js
@@ -0,0 +1,85 @@
+'use strict';
+
+const uniID = require('uni-id')
+
+exports.main = async (event, context) => {
+    //event为客户端上传的参数
+    console.log('event : ' + event)
+
+
+    let params = event.params
+    let res = {}
+
+    let payload = {}
+
+    switch (event.action) {
+        case 'register':
+            res = uniID.register(event.params, context);
+            break;
+        case 'login':
+            res = uniID.login(event.params, context);
+            break;
+        case 'logout':
+            payload = await uniID.checkToken(event.uniIdToken)
+            console.log('index::payload:', payload);
+
+            if (payload.code && payload.code > 0) {
+                return payload
+            }
+            res = logout(payload.uid, context);
+            break;
+        case 'updatePassword':
+            payload = await uniID.checkToken(event.uniIdToken)
+            console.log('index::payload:', payload);
+
+            if (payload.code && payload.code > 0) {
+                return payload
+            }
+            params.uid = payload.uid
+
+            res = updatePwd(params, context);
+            break;
+        case 'setAvatar':
+            payload = await uniID.checkToken(event.uniIdToken)
+            console.log('index::payload:', payload);
+
+            if (payload.code && payload.code > 0) {
+                return payload
+            }
+            params.uid = payload.uid
+
+            res = uniID.setAvatar(params, context);
+            break;
+        case 'bindMobile':
+            payload = await uniID.checkToken(event.uniIdToken)
+            console.log('index::payload:', payload);
+
+            if (payload.code && payload.code > 0) {
+                return payload
+            }
+            params.uid = payload.uid
+
+            res = uniID.bindMobile(params, context);
+            break;
+        case 'bindEmail':
+            payload = await uniID.checkToken(event.uniIdToken)
+            console.log('index::payload:', payload);
+
+            if (payload.code && payload.code > 0) {
+                return payload
+            }
+            params.uid = payload.uid
+
+            res = uniID.bindEmail(params, context);
+            break;
+        default:
+            res = {
+                code: 403,
+                msg: '非法访问'
+            }
+            break;
+    }
+
+    //返回数据给客户端
+    return res
+};
